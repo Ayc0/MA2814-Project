a45727208b70144d9d2e8479ddd843c266bcc0ef
Ayc0 <benjamin.koltes@gmail.com>
2018-06-26 20:00:33 +0200

    complete import

--- README.md
+++ README.md
@@ -10,6 +10,12 @@ Project for MA2814 - Introduction to Random Modeling
 
 - `pipenv install`
 
+# Commands
+
+- `pipenv run import <filename> [-o, --override]` <br>
+  Parse `sources/<filename>.txt` and compute its related markov chain, then stores it in `.cache` folder.<br><br>
+  if `<filename>` is already cached, and `override` isn't specified, it just sets this markov chain as the current one
+
 # Bibliography
 
 [Notre-Dame de Paris](https://fr.wikisource.org/wiki/Notre-Dame_de_Paris/)
--- src/import.py
+++ src/import.py
@@ -1,24 +1,67 @@
 import os
 import sys
+import pickle
+import shutil
 
 from settings import SPACE
 from markov_chain import MarkovChain
 
-source_dir = os.path.join(os.path.dirname(__file__), 'sources')
+source_dir = os.path.join(os.path.dirname(__file__), '../sources')
+cache_dir = os.path.join(os.path.dirname(__file__), '../.cache')
 
-if len(sys.argv) < 2:
-    raise Exception('Must give a filename')
+def stop(message, error=True):
+    if error:
+        print('Error:', end=" ")
+    print(message)
+    sys.exit(1)
 
-filename = "{}.txt".format(sys.argv[1])
+# Setup input
+
+override = False
+
+if '--override' in sys.argv or '-o' in sys.argv:
+    override = True
+    argv = [x for x in sys.argv if x != '-o' and x != '--override']
+else:
+    argv = sys.argv
+
+if len(argv) < 2:
+    stop('Must give a filename')
+
+file = argv[1]
+filename = "{}.txt".format(file)
+
+if file == "current":
+    stop('"current" isn\'t a valid filename')
 
 available_files = os.listdir(source_dir)
 
 if filename not in available_files:
-    raise Exception('{} isn\'t a valid file'.format(sys.argv[1]))
+    stop('{} isn\'t a valid file'.format(file))
 
 source_filename = os.path.join(source_dir, filename)
 
+# Detect whether or not the MC should be computed
+
+# Create cache folder if doesn't exists
+cache_folder_exists = os.path.isdir(cache_dir)
+if not(cache_folder_exists):
+    print("Create cache folder")
+    os.makedirs(cache_dir)
+
+cache_filename = os.path.join(cache_dir, "{}.pkl".format(file))
+cache_current = os.path.join(cache_dir, "current.pkl")
+cache_file_exists = os.path.exists(cache_filename)
+
+if not(override) and cache_file_exists:
+    shutil.copy(cache_filename, cache_current)
+    stop('Swap current markov chain to "{}"'.format(file), error=False)
+
+# Compute MC
+
 markov_chain = MarkovChain()
+
+print('Parsing file "{}"...'.format(file))
 
 with open(source_filename, 'r') as f:
     word = f.readline()
@@ -31,3 +74,12 @@ with open(source_filename, 'r') as f:
         word = f.readline()
 
 markov_chain.build_array()
+
+# Store MC
+with open(cache_filename, 'wb') as f:
+    pickle.dump(markov_chain, f)
+
+shutil.copy(cache_filename, cache_current)
+
+print("Parsing done.")
+print('Set "{}" as current markov chain'.format(file))
