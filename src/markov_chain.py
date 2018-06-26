cd93172dbe524b6ab0a3ea13401bcf028b734400
Ayc0 <benjamin.koltes@gmail.com>
2018-06-27 00:55:05 +0200

    Add functions

--- src/markov_chain.py
+++ src/markov_chain.py
@@ -1,4 +1,5 @@
 import numpy as np
+from random import random
 
 from settings import SPACE
 
@@ -8,30 +9,56 @@ def set_to_list(s):
     return array
 
 
-
 class MarkovChain:
-    def __init__(self):
-        self.markov_chain_dict = dict()
-        self.markov_chain_array = None
-        self.markov_chain_matrix = None
-        self.letters = { SPACE }
+    def __init__(self, letters={ SPACE }, letters_table=dict(), transition_array=None, transition_matrix=None):
+        self.letters = letters
+        self.letters_table = letters_table
+        self.transition_array = transition_array
+        self.transition_matrix = transition_matrix
 
     def add(self, prev, new):
         self.letters.add(prev)
         self.letters.add(new)
-        if ((prev, new) in self.markov_chain_dict):
-            self.markov_chain_dict[(prev, new)] += 1
+        if ((prev, new) in self.letters_table):
+            self.letters_table[(prev, new)] += 1
         else:
-            self.markov_chain_dict[(prev, new)] = 1
+            self.letters_table[(prev, new)] = 1
     
     def get(self, prev, new):
-        if self.markov_chain_dict.get((prev, new)) is not None:
-            return self.markov_chain_dict[(prev, new)]
+        if self.letters_table.get((prev, new)) is not None:
+            return self.letters_table[(prev, new)]
         return 0
     
     @property
     def letters_list(self):
         return set_to_list(self.letters)
+    
+    @property
+    def letters_array(self):
+        return np.array(self.letters_list)
+    
+    def sanitize(self, letters):
+        return set(letters) & self.letters
+
+    def rowOfLetters(self, letters):
+        letters = list(self.sanitize(letters))
+        array = letters_array * 0
+        for letter in letters:
+            array += self.letters_array == letter
+        return array / len(letters)
+
+    def nextProba(self, rowOfLetters, order=1):
+        return rowOfLetters * (self.transition_matrix ** order)
+
+    def nextLetter(self, letter):
+        rowOfLetters = self.rowOfLetters(letter)
+        rowOfLetters = self.nextProba(rowOfLetters)
+        rand = random()
+        cumulative_sum = 0
+        for (index, letter) in enumerate(self.letters_list):
+            cumulative_sum += rowOfLetters[0, index]
+            if cumulative_sum > rand:
+                return letter
 
     def build(self):
         letters_list = self.letters_list
@@ -41,8 +68,8 @@ class MarkovChain:
             for letterB in letters_list:
                 row.append(self.get(letterA, letterB))
             table.append(row)
-        self.markov_chain_array = np.array(table)
-        markov_chain_array = self.markov_chain_array / self.markov_chain_array.sum(axis=1)[:,None]
-        self.markov_chain_matrix = np.matrix(markov_chain_array)
+        self.transition_array = np.array(table)
+        transition_array = self.transition_array / self.transition_array.sum(axis=1)[:,None]
+        self.transition_matrix = np.matrix(transition_array)
 
 
